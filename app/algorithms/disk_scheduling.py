823e8477beb203407614687de3957e0cdc6e3374
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.ph>
2026-06-24 10:32:52 +0800

    sstf

--- app/algorithms/disk_scheduling.py
+++ app/algorithms/disk_scheduling.py
@@ -8,3 +8,16 @@ def fcfs_disk (request, initial_head):
 
         current_head = req
     return head_movement
+
+def sstf_disk(requests, initial_head):
+    remaining_reqs = list(requests)
+    total_head_movement = 0
+    current_head = initial_head
+    
+    while len(remaining_reqs) > 0:       
+        closest_req = min(remaining_reqs, key=lambda track: abs(track - current_head))      
+        total_head_movement += abs(closest_req - current_head)              
+        current_head = closest_req        
+        remaining_reqs.remove(closest_req)
+        
+    return total_head_movement
