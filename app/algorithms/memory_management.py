c6fbb3dc1c5bb3a116ae6d43f4502ff1712b177f
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.ph>
2026-06-24 22:26:31 +0800

    update: memory management without compaction

--- app/algorithms/memory_management.py
+++ app/algorithms/memory_management.py
@@ -1,53 +1,4 @@
-def first_fit (blocks, processes):
-    free_blocks = list(blocks)
-    allocation = [-1] * len(processes)
-
-    for i in range(len(processes)):
-        for j in range(len(free_blocks)):
-            if free_blocks >= processes[i]:
-                allocation[i] = j
-                free_blocks -= processes[i]
-                break
-    return allocation
-
-def best_fit (blocks, processes):
-    free_blocks = list(blocks)
-    allocation = [-1] * len(processes)
-
-    for i in range (len(processes)):
-        best_block = -1
-        min_block = float('inf')
-
-        for j in range(len(free_blocks)):
-            if free_blocks[j] >= processes[i]:
-                if free_blocks[j] < min_block:
-                    min_block = free_blocks[j]
-                    best_block = j
-        if best_block != -1:
-            allocation[i] = best_block
-            free_blocks[best_block] -= processes[i]
-    return allocation
-
-
-def worst_fit (blocks, processes):
-    free_blocks = list(blocks)
-    allocation = [-1] * len(processes)
-
-    for i in range(len(processes)):
-        best_blocks = -1
-        max_block = -1
-
-        for j in range(len(free_blocks)):
-            if free_blocks[j] >= processes[i] and free_blocks[j] > max_block:
-                max_block = free_blocks[j]
-                best_blocks = j
-            
-            if best_blocks != -1:
-                allocation[i] = best_blocks
-                free_blocks[best_blocks] -= processes[i]
-    return allocation
-
-def mm_with_compacxtion (blocks, processes, strategy = "first"):
+def mm_with_compaction (blocks, processes, strategy = "first"):
     free_blocks = list(blocks)
     allocation = [-1] * len (processes)
 
@@ -87,6 +38,47 @@ def mm_with_compacxtion (blocks, processes, strategy = "first"):
                 allocation = -1
     return {"allocation": allocation, "final_blocks": free_blocks}
             
+def mm_without_compaction(blocks, processes, strategy="first"):
+    free_blocks = list(blocks)
+    allocation = [-1] * len(processes)
+
+    for i in range(len(processes)):
+        
+        req_size = processes[i]
+        recent_process = -1
+
+        if strategy == "first":
+            for j in range(len(free_blocks)):
+                if free_blocks[j] >= req_size:
+                    recent_process = j
+                    break
+
+        elif strategy in ["best", "best_available"]:
+            min_size = float('inf')
+            for j in range(len(free_blocks)):
+                if free_blocks[j] >= req_size and free_blocks[j] < min_size:
+                    min_size = free_blocks[j]
+                    recent_process = j
+        
+        elif strategy == "worst":
+            max_size = -1
+            for j in range(len(free_blocks)):
+                if free_blocks[j] >= req_size and free_blocks[j] > max_size:
+                    max_size = free_blocks[j]
+                    recent_process = j
+        
+        if recent_process != -1:
+            allocation[i] = recent_process
+            free_blocks[recent_process] -= req_size
+        
+        else:
+            allocation[i] = -1
+    return {"allocation": allocation, "final_blocks": free_blocks}
+            
+
+        
+
+
                                     
 
 
