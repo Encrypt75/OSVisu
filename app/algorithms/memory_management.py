feb7e6b9744d952c2025fcd3aa712d51b2ef6ad0
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.ph>
2026-06-24 09:32:19 +0800

    best fit

--- app/algorithms/memory_management.py
+++ app/algorithms/memory_management.py
@@ -10,4 +10,25 @@ def first_fit (blocks, processes):
                 break
     return allocation
 
+def best_fit (blocks, processes):
+    free_blocks = list(blocks)
+    allocation = [-1] * len(processes)
+
+    for i in range (len(processes)):
+        best_block = -1
+        min_block = float('inf')
+
+        for j in range(len(free_blocks)):
+            if free_blocks[j] >= processes[i]:
+                if free_blocks[j] < min_block:
+                    min_block = free_blocks[j]
+                    best_block = j
+        if best_block != -1:
+            allocation[i] = best_block
+            free_blocks[best_block] -= processes[i]
+    return allocation
+
+
+
+
 
