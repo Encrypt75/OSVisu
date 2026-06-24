eacc794888a1be1705fb5a2c8caa936d1bee36fc
iskenneth <kennethgserrano@iskolarngbayan.pup.edu.ph>
2026-06-24 16:49:02 +0800

    disk scheduling

--- app/algorithms/disk_scheduling.py
+++ app/algorithms/disk_scheduling.py
@@ -21,3 +21,70 @@ def sstf_disk(requests, initial_head):
         remaining_reqs.remove(closest_req)
         
     return total_head_movement
+def disk_scheduling (requests, initial_head, disk_size = 200, direction='right', strategy="fcfs"):
+    total_movement = 0
+    current_head = initial_head
+    sequence = []
+
+    if strategy == "fcfs":
+        for req in requests:
+            distance = abs(req - current_head)
+            total_movement += distance
+            sequence.append(req)
+            current_head = req
+    elif strategy == "sstf":
+        remaining_reqs = list(requests)
+        while len(remaining_reqs) > 0:
+            closest_req = min(remaining_reqs, key=lambda track: abs(track - current_head))
+            total_movement += abs(closest_req - current_head)
+            sequence.append(closest_req)
+            current_head = closest_req
+            remaining_reqs.remove(closest_req)       
+    else: 
+        left = [r for r in requests if r < initial_head]
+        right = [r for r in requests if r >= initial_head]
+    
+    if strategy in ["scan", "look"]:
+        left.sort(reverse=True)
+        right.sort()
+        
+        for run in range(2):
+            if direction == "right":
+                for req in right:
+                    sequence.append(req)
+                    total_movement += abs(req - current_head)
+                    current_head = req
+                if strategy == "scan" and left:
+                    end_track = disk_size - 1
+                    total_movement += abs(end_track - current_head)
+                    current_head = end_track
+                direction = "left"
+            elif direction == "left":
+                for req in left:
+                    sequence.append(req)
+                    total_movement += abs(req - current_head)
+                    current_head = req
+                    
+                if strategy == "scan" and right and current_head != 0:
+                    total_movement += abs(0 - current_head)
+                    current_head = 0
+                
+                direction = "right"
+    elif strategyin ["c_scan", "c_look"]:
+        left.sort()
+        right.sort()
+
+        for req in right:
+            sequence.append(req)
+            total_movement += abs(req - current_head)
+            current_head = req
+        
+        if left:
+            if strategy == "c_scan":
+                end_track = disk_size - 1
+                total_head_movement += abs(end_track - current_head)
+                total_head_movement += abs(end_track - 0)
+                current_head = 0
+
+        
+
